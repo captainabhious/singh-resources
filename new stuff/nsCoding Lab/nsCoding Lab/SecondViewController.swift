158413544c17933b35b5bb1a7c53b75826b60ea4
captainabhious <captainabhious@gmail.com>
2017-10-17 01:38:14 -0700

    new stuff

---
+++ new stuff/nsCoding Lab/nsCoding Lab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  nsCoding Lab
+//
+//  Created by Abhi Singh on 10/16/17.
+//  Copyright © 2017 Abhi Singh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
