c09acf3c933b314d800f49a4fb556eff86760d0e
captainabhious <captainabhious@gmail.com>
2018-01-04 20:38:57 -0800

    functions - swift programming lang guide

---
+++ Functions:Closures/SLG - Closures.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+// CLOSURES: self-contained blocks of functionality that can be passed around & used in code
+// - can capture and store references to any constants and variables from the context in which they're defined 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
